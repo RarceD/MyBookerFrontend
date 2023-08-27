3f99fab8552374d0b44b574d1e5c0f50a8978c65
RarceD <akimbo170@gmail.com>
2023-08-27 12:47:32 +0200

    Update stats info dto

--- src/interfaces/StatsDto.ts
+++ src/interfaces/StatsDto.ts
@@ -2,3 +2,10 @@ export interface StatsDto {
     id: number,
     token: string
 }
+
+export interface StatsInfo {
+    success: boolean,
+    date: string,
+    count?: number,
+    countNOK? : number
+}
