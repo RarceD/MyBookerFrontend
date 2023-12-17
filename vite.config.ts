fb5c549ee800f441e6d6f2596f874a44b1b4cd02
RarceD <akimbo170@gmail.com>
2023-12-17 08:46:06 +0100

    Force run port 8000

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import viteCompression from 'vite-plugin-compression';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteCompression()],
+  server: {
+    host: true,
+    port: 8000,
+    watch: {
+      usePolling: true
+    }
+  }
 })
